cb4bfbcac2d319c4c832da0505b29dbd8beb1768
Inker Kuo <inker610566@yahoo.com.tw>
2015-01-25 18:15:34 +0800

    okok

---
+++ sendinput.py
@@ -0,0 +1,30 @@
+from ctypes  import c_int, WINFUNCTYPE, windll
+from ctypes.wintypes import HWND, LPCSTR, UINT, LPARAM, BOOL
+import ctypes
+
+#print ctypes.windll.user32.SendInput
+#print hex(ctypes.windll.User32.FindWindowExA(None, None, None, None))
+#hwnd = ctypes.windll.User32.FindWindowExA(None, None, None, None)
+
+# c_char_p
+#print ctypes.windll.User32.GetClassNameA(hwnd, p, 100)
+
+callback_proto = WINFUNCTYPE(BOOL, HWND, LPARAM)
+
+def EnumWindowsCallback(hwnd, lparam):
+	p = ctypes.cdll.msvcrt.malloc(c_int(100))
+	ctypes.windll.User32.GetWindowTextA(hwnd, p, 100)
+	ctypes.cdll.msvcrt.printf(p)
+	print ""
+	ctypes.cdll.msvcrt.free(p)
+
+callback = callback_proto(EnumWindowsCallback)
+
+ctypes.windll.User32.EnumWindows(callback, 0)
+
+
+
+'''
+windows native type
+["ARRAY", "ATOM", "ArgumentError", "Array", "BOOL", "BOOLEAN", "BYTE", "BigEndianStructure", "CDLL", "CFUNCTYPE", "COLORREF", "DEFAULT_MODE", "DOUBLE", "DWORD", "DllCanUnloadNow", "DllGetClassObject", "FILETIME", "FLOAT", "FormatError", "GetLastError", "HACCEL", "HANDLE", "HBITMAP", "HBRUSH", "HCOLORSPACE", "HDC", "HDESK", "HDWP", "HENHMETAFILE", "HFONT", "HGDIOBJ", "HGLOBAL", "HHOOK", "HICON", "HINSTANCE", "HKEY", "HKL", "HLOCAL", "HMENU", "HMETAFILE", "HMODULE", "HMONITOR", "HPALETTE", "HPEN", "HRESULT", "HRGN", "HRSRC", "HSTR", "HTASK", "HWINSTA", "HWND", "INT", "LANGID", "LARGE_INTEGER", "LCID", "LCTYPE", "LGRPID", "LONG", "LPARAM", "LPCOLESTR", "LPCSTR", "LPCVOID", "LPCWSTR", "LPOLESTR", "LPSTR", "LPVOID", "LPWSTR", "LibraryLoader", "LittleEndianStructure", "MAX_PATH", "MSG", "OLESTR", "OleDLL", "POINT", "POINTER", "POINTL", "PYFUNCTYPE", "PyDLL", "RECT", "RECTL", "RGB", "RTLD_GLOBAL", "RTLD_LOCAL", "SC_HANDLE", "SERVICE_STATUS_HANDLE", "SHORT", "SIZE", "SIZEL", "SMALL_RECT", "SetPointerType", "Structure", "UINT", "ULARGE_INTEGER", "ULONG", "USHORT", "Union", "VARIANT_BOOL", "WCHAR", "WIN32_FIND_DATAA", "WIN32_FIND_DATAW", "WINFUNCTYPE", "WORD", "WPARAM", "WinDLL", "WinError", "_COORD", "_FILETIME", "_LARGE_INTEGER", "_POINTL", "_RECTL", "_SMALL_RECT", "_SimpleCData", "_ULARGE_INTEGER", "__all__", "__builtins__", "__doc__", "__file__", "__name__", "__package__", "addressof", "alignment", "byref", "c_bool", "c_buffer", "c_byte", "c_char", "c_char_p", "c_double", "c_float", "c_int", "c_int16", "c_int32", "c_int64", "c_int8", "c_long", "c_longdouble", "c_longlong", "c_short", "c_size_t", "c_ssize_t", "c_ubyte", "c_uint", "c_uint16", "c_uint32", "c_uint64", "c_uint8", "c_ulong", "c_ulonglong", "c_ushort", "c_void_p", "c_voidp", "c_wchar", "c_wchar_p", "cast", "cdll", "create_string_buffer", "create_unicode_buffer", "get_errno", "get_last_error", "memmove", "memset", "oledll", "pointer", "py_object", "pydll", "pythonapi", "resize", "set_conversion_mode", "set_errno", "set_last_error", "sizeof", "string_at", "tagMSG", "tagPOINT", "tagRECT", "tagSIZE", "windll", "wstring_at"]
+'''
